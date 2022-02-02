c2abf965ccedea9847581d64b969d603f84444a4
as2457 <adam.smith.home@ntlworld.com>
2022-02-02 09:54:27 +0000

    slight improvement to measurement

--- Sources/quantumCircuit/states.swift
+++ Sources/quantumCircuit/states.swift
@@ -1,4 +1,4 @@
-
+import Foundation
 
 public class QuantumState {
 
@@ -28,18 +28,48 @@ public class QuantumState {
         var probabilities: [Double] = (state .* state.conj).real
         probabilities = cumsum(probabilities)
         
-        var measuredStateList: [Int] = []
-        for _ in 0..<shots {
-            let r_val = Double.random(in: 0..<1)
-            var x: [Int] = Array(0..<probabilities.count)
-            x = x.map{ (probabilities[$0]<r_val ? 1 : 0) }
-            let whichState: Int = x.reduce(0){ $0 + $1 } //ones.reduce(0){ $0 + (probabilities[$1]<r_val ? $1 : 0 ) }
-            measuredStateList = measuredStateList + [whichState]  //need to convert to strings!
-        }
+        let x: [Int] = Array(0..<probabilities.count)
         
         var measurementDict: [String : Int] = [:]
+        var measuredStateList: [Int] = []
         
-        for whichState in measuredStateList {
+        let syncQueue = DispatchQueue(label: "...") // needed when accessing a variable from many threads
+
+        // Parellization not worth it! Do differently.
+        //DispatchQueue.concurrentPerform(iterations: shots) { kk in
+        //for _ in 0..<shots {
+            let r_val = Double.random(in: 0..<1)
+            
+            var whichState: Int = probabilities.count-1
+            //var accum = 0.0
+            for (i, p) in probabilities.enumerated() {
+                //accum += p
+                if r_val < p {
+                    whichState = i 
+                    break
+                }
+            }
+
+            //let y = x.map{ (probabilities[$0]<r_val ? 1 : 0) }
+            //let whichState: Int = y.reduce(0, +) //ones.reduce(0){ $0 + (probabilities[$1]<r_val ? $1 : 0 ) }
+            //measuredStateList = measuredStateList + [whichState]  //need to convert to strings!
+            
+            var stateString = num2bin(num: whichState, toSize: self.N)
+            stateString = qubits.compactMap{ stateString[$0] }.joined()
+
+            syncQueue.sync {
+                if measurementDict[stateString] != nil {
+                    measurementDict[stateString]! += 1
+                } else {
+                    measurementDict[stateString] = 1
+                }
+            }
+
+        }
+        
+        
+        
+        /*for whichState in measuredStateList {
             var stateString = num2bin(num: whichState, toSize: self.N)
             stateString = qubits.compactMap{ stateString[$0] }.joined()
             if measurementDict[stateString] != nil {
@@ -47,7 +77,7 @@ public class QuantumState {
             } else {
                 measurementDict[stateString] = 1
             }
-        }
+        }*/
         
         return measurementDict
     }
